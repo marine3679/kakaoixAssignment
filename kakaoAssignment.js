08e500122c32bb9dab465d1e25d433be3ef25aea
Joey <marine0436@gmail.com>
2018-09-02 18:08:28 +0900

    add comment

--- kakaoAssignment.js
+++ kakaoAssignment.js
@@ -1,6 +1,9 @@
+// 아파치 얼굴 취득
 var layout = $("#kimLayout")
 
+// 아파치 얼굴을 클릭
 layout.click(function(ev) {
+  //로그인, 인스타그램, FAQ에 직접 갈수 있는 위젯 생성
   $("#kimLogin").toggleClass("widget-move1", 100, "linear");
   $("#kimInstagram").toggleClass("widget-move2", 100, "linear");
   $("#kimFaq").toggleClass("widget-move3", 100, "linear");
@@ -9,4 +12,5 @@ layout.click(function(ev) {
   .css("display", "");
 });
 
+//아파치는 언제든지 드래그가 가능함.
 layout.draggable();
